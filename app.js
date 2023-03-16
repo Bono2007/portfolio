7df7a653e5a0dc55697b0aef936fdfdf2b58524a
bono2007 <llc314159@gmail.com>
2023-03-16 11:45:02 +0100

    works.html : 2 images ou 1 vidéo

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ const span = document.getElementsByClassName("close")[0];
 const modalTitle = document.getElementById("modal-title");
 const modalDescription = document.getElementById("modal-description");
 const modalImage = document.getElementById("modal-image");
+const modalImage2 = document.getElementById("modal-image2");
+const modalVideo = document.getElementById("modal-video");
+const modalSource = document.getElementById("modal-source");
 
 // On boucle à travers tous les boutons "More" pour leur ajouter un gestionnaire d'événements onclick
 const buttons = document.querySelectorAll(".legende-button");
@@ -30,10 +33,26 @@ for (let i = 0; i < buttons.length; i++) {
     const title = buttons[i].getAttribute("data-title");
     const description = buttons[i].getAttribute("data-description");
     const image = buttons[i].getAttribute("data-image");
+    const image2 = buttons[i].getAttribute("data-image2");
+    const video = buttons[i].getAttribute("data-video");
     // Afficher le modal et mettre à jour les éléments HTML avec les informations de la vignette
     modalTitle.textContent = title;
     modalDescription.textContent = description;
+    //Partie image si présente :
     modalImage.src = image;
+    //Partie image2 si présente
+    modalImage2.src = image2;
+    modalImage2.style.display = "none";
+    if (image2) { //si la vignette contient une 2e image
+      modalImage2.style.display = "block"; //afficher la 2e image
+    }
+    //Partie vidéo si présente :
+    modalVideo.style.display = "none";
+    if (video) { // si la vignette contient une vidéo
+      modalSource.src = video;
+      modalVideo.style.display = "block"; // afficher la vidéo
+      modalVideo.load(); // charger la vidéo
+    }
     modal.style.display = "block";
   }
 }
