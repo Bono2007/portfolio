6e25eac902dc86d729a96f26f0756ee24d4bf351
bono2007 <llc314159@gmail.com>
2023-03-16 12:46:54 +0100

    Correction legal notice tout en bas et rajout responsive iphone11

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-function showImage(button) {
-    const imageSrc = button.getAttribute('data-image'); //Transforme l'image en bouton
-    const overlay = document.querySelector('.overlay');
-    const overlayImage = document.querySelector('.overlay-image');
-    overlay.addEventListener('click', hideImage);  //Ajoute un écouteur d'événement qui va lancer la fonction hideImage() au clic
+// function showImage(button) {
+//     const imageSrc = button.getAttribute('data-image'); //Transforme l'image en bouton
+//     const overlay = document.querySelector('.overlay');
+//     const overlayImage = document.querySelector('.overlay-image');
+//     overlay.addEventListener('click', hideImage);  //Ajoute un écouteur d'événement qui va lancer la fonction hideImage() au clic
     
-    overlayImage.src = imageSrc;
-    overlay.classList.add('show');
-    overlay.classList.remove('hide');
-  }
+//     overlayImage.src = imageSrc;
+//     overlay.classList.add('show');
+//     overlay.classList.remove('hide');
+//   }
 
-  function hideImage() {
-    const overlay = document.querySelector('.overlay');
-    overlay.classList.add('hide');
-    overlay.classList.remove('show');
-  }
+//   function hideImage() {
+//     const overlay = document.querySelector('.overlay');
+//     overlay.classList.add('hide');
+//     overlay.classList.remove('show');
+//   }
 
   // Le JavaScript pour afficher ou cacher le modal
 const modal = document.getElementById("modal-container");
